26f212050236a36b6bba5fc7ec24c918cc524344
tikaaay <94545146+tikaaay@users.noreply.github.com>
2022-04-03 23:58:01 +0200

    Add files via upload

--- SITE OPTIMISATION/app.js
+++ SITE OPTIMISATION/app.js
@@ -1,18 +1,22 @@
 const nav = document.getElementById("container-nav");
-const vraiNav = document.querySelector("nav")
+const vraiNav = document.querySelector("nav");
 const header = document.querySelector("header");
-const filante = document.getElementById("filante")
-const returnUp = document.getElementById("return-up")
+const filante = document.getElementById("filante");
+const returnUp = document.getElementById("return-up");
 
 window.addEventListener("load", () => {
     const TimeLine =gsap.timeline({paused: true});
     TimeLine.from(header, 1, {opacity: 0} )
-    TimeLine.from(filante, 1, {left: -600}, 0.2)
-    TimeLine.staggerFrom(nav, 1, {top: -70})
-    TimeLine.staggerFrom(returnUp, 1, {opacity: 0}, 0.3)
+    TimeLine.from(filante, 1, {left: -600}, "-=1")
+    TimeLine.staggerFrom(nav, 1, {top: -70}, "-=1")
+    TimeLine.staggerFrom(returnUp, 1, {opacity: 0}, 0.3, "-=1")
 
 
     TimeLine.play();
+})
+
+ancres.addEventListener("click", () => {
+
 })
 
 // window.addEventListener("scroll", () => {

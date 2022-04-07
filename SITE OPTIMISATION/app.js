765298f197e30cbb47f6b4f16a93aca933e8cf2d
tikaaay <94545146+tikaaay@users.noreply.github.com>
2022-04-07 17:58:39 +0200

    Add files via upload

---
+++ SITE OPTIMISATION/app.js
@@ -0,0 +1,31 @@
+const nav = document.getElementById("container-nav");
+const vraiNav = document.querySelector("nav");
+const header = document.querySelector("header");
+const filante = document.getElementById("filante");
+const returnUp = document.getElementById("return-up");
+
+window.addEventListener("load", () => {
+    const TimeLine =gsap.timeline({paused: true});
+    TimeLine.from(header, 1, {opacity: 0} )
+    TimeLine.from(filante, 1, {left: -600}, 0.7, "-=1")
+    TimeLine.staggerFrom(nav, 1, {top: -70}, "-=1")
+    TimeLine.staggerFrom(returnUp, 1, {opacity: 0}, 0.3, "-=1")
+
+
+    TimeLine.play();
+})
+
+ancres.addEventListener("click", () => {
+
+})
+
+// window.addEventListener("scroll", () => {
+//     if (window.scrollY > 30) {
+//         nav.classList.add("anim-nav");
+//         vraiNav.style.position ="fixed";
+        
+//     } else {
+//         nav.classList.remove("anim-nav");
+//         vraiNav.style.position ="initial";
+//     }
+// })
